0f794a89c3ccda497b20d11a00d121014f2a393a
ashika_hzain <ashikahussain111@gmail.com>
2021-05-20 21:04:27 +0530

    gallery

---
+++ gallery_assets/js/main.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+// Porfolio isotope and filter
+$(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox({
+        'share': false
+      });
+    });
+  });
+})(Jquery);
